2f4474801b7e04116a56dbc1e588a3637704853d
German Muises <germanmuises@hotmail.com>
2020-03-01 13:18:26 -0300

    correcciones obtencion de lineas

--- nextUcodBaseJuego/js/app.js
+++ nextUcodBaseJuego/js/app.js
@@ -15,12 +15,12 @@ $(document).ready(function() {
 
 //funcion que llena el tablero aleatoriamente al iniciar
 function llenarTablero(){
-  var columnas = $("div[class^='col']");
-  for (var i = 1; i <= 7; i++) {
+  var columnas = $(".panel-tablero div[class^='col-']")
+  for (var i = 0; i < columnas.length; i++) {
     var x = 1;
     while (x <= 7) {
       var numImagen = Math.floor((Math.random() * 4) + 1);
-      $(".col-"+x).append("<div class='row-'"+i+"><img src='image/"+numImagen+".png' class='dulce'/></div>");
+      $(columnas[i]).append("<div class='row-"+x+"'><img src='image/"+numImagen+".png' class='dulce'/></div>")
       x++;
     }
   }
@@ -29,26 +29,26 @@ function llenarTablero(){
 //funcion que obtiene lineas de columna y filas para recorrer
 function obtenerLinea(arreglo, indice){
 
-  var columna1 = $('.col-1').children();
-  var columna2 = $('.col-2').children();
-  var columna3 = $('.col-3').children();
-  var columna4 = $('.col-4').children();
-  var columna5 = $('.col-5').children();
-  var columna6 = $('.col-6').children();
-  var columna7 = $('.col-7').children();
+  var columna1 = $('.panel-tablero .col-1').children();
+  var columna2 = $('.panel-tablero .col-2').children();
+  var columna3 = $('.panel-tablero .col-3').children();
+  var columna4 = $('.panel-tablero .col-4').children();
+  var columna5 = $('.panel-tablero .col-5').children();
+  var columna6 = $('.panel-tablero .col-6').children();
+  var columna7 = $('.panel-tablero .col-7').children();
 
   var arregloColumnas = $([columna1, columna2, columna3, columna4, columna5, columna6, columna7]);
 
-  var fila1 = "", fila2 = "", fila3 = "",fila4 = "", fila5 = "", fila6 = "", fila7 = "";
-  var arregloFilas = $([fila1, fila2, fila3, fila4, fila5, fila5, fila7]);
+  var fila1 = $('.panel-tablero .row-1');
+  var fila2 = $('.panel-tablero .row-2');
+  var fila3 = $('.panel-tablero .row-3');
+  var fila4 = $('.panel-tablero .row-4');
+  var fila5 = $('.panel-tablero .row-5');
+  var fila6 = $('.panel-tablero .row-6');
+  var fila7 = $('.panel-tablero .row-7');
 
-  for (var i = 0; i < 7; i++) {
-    var filas = $([fila1, fila2, fila3, fila4, fila5, fila5, fila7]);
-    for (var j = 0; j < 7; j++) {
-      filas[j] = $([arregloColumnas[i].children(j)]);
-    }
-    arregloFilas = filas;
-  }
+  var arregloFilas = $([fila1, fila2, fila3, fila4, fila5, fila6, fila7]);
+
 
   if (arreglo === 'columns') {
     return arregloColumnas;
